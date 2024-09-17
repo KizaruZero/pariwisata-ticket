4036422cf29886ba13421f0bd1d1db652d023a99
Ardya Pusaka <143238697+KizaruZero@users.noreply.github.com>
2024-09-17 22:06:09 +0700

    your

--- app/Filament/Widgets/statistikTrip.php
+++ app/Filament/Widgets/statistikTrip.php
@@ -14,20 +14,17 @@ class statistikTrip extends BaseWidget
     {
         return [
             Stat::make('Total Destinations', Destination::count())
-                ->value(10)
                 ->description('Total Destinasi Wisata yang terdaftar')
                 ->descriptionIcon('heroicon-o-map-pin') // Gunakan ikon yang benar
                 ->chart([1, 3, 5, 10, 20, 40])
                 ->color('success'),
             Stat::make('Total Region/Wilayah', Region::count())
                 ->description('Total Region/Wilayah yang terdaftar')
-                ->value(10)
                 ->descriptionIcon('heroicon-o-map') // Gunakan ikon yang benar
                 ->chart([1, 3, 5, 10, 20, 40])
                 ->color('success'),
             Stat::make('Total Kategori', Category::count())
                 ->description('Total Kategori yang terdaftar')
-                ->value(10)
                 ->descriptionIcon('heroicon-o-map-pin') // Gunakan ikon yang benar
                 ->chart([1, 3, 5, 10, 20, 40])
                 ->color('success'),
