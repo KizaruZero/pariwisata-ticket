feda292e064c002b595a99a838a06eae9a88ba3c
notcikken <cyrullusikkendevstiandra@gmail.com>
2024-11-29 09:17:26 +0700

    UPDATE tampilan login, register

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -68,128 +68,145 @@ const submit = () => {
     <Head title="Log in" />
 
     <img src="../../assets/home.png" class="absolute w-screen h-screen z-10" />
-    <main
-        class="flex absolute inset-0 shadow-2xl bg-white/20 backdrop-blur-3xl ml-[750px] mt-screen rounded-[30px] w-[700px] h-[600px] m-auto z-50"
-    >
-        <section
-            class="flex flex-col px-16 mb-0 w-full text-2xl leading-none max-md:px-5 h-[100px] rounded-[50px]"
-        >
-            <header
-                class="flex flex-row items-end -ml-10 mt-4 max-w-full text-2xl leading-none w-[89px]"
-            >
-                <Link :href="route('home')" :active="route().current('home')">
+    <div class="flex flex-row w-screen h-screen">
+    <div class="flex w-1/2">
+        <!-- Left content -->
+    </div>
+    <div class="flex w-1/2 justify-center items-center">
+        <main class="flex shadow-2xl bg-white/20 backdrop-blur-3xl rounded-[30px] w-[700px] h-[650px] z-50">
+            
+                <section
+                    class="flex flex-col px-16 mb-0 w-full text-2xl leading-none max-md:px-5 h-[100px] rounded-[50px]"
+                >
+                    <header
+                        class="flex flex-row items-end -ml-10 mt-6 max-w-full text-2xl leading-none w-[89px]"
+                    >
+                        <Link :href="route('home')" :active="route().current('home')">
+                            <img
+                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/2cd9ddf8843928bbd676bc009af63527de529dc32fabc9853b551aff413055c9?placeholderIfAbsent=true&apiKey=ed89e008b7de47e5a3187e8a4c1c72f3"
+                                alt=""
+                                class="object-contain self-start w-12 aspect-[0.94]"
+                            />
+                        </Link>
+                    </header>
+
                     <img
-                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/2cd9ddf8843928bbd676bc009af63527de529dc32fabc9853b551aff413055c9?placeholderIfAbsent=true&apiKey=ed89e008b7de47e5a3187e8a4c1c72f3"
-                        alt=""
-                        class="object-contain self-start w-12 aspect-[0.94]"
+                        src="../../assets/logo/vagabond.svg"
+                        alt="Vagabond logo"
+                        class="object-contain mx-auto -mt-1 max-w-full aspect-[1.63] w-[125px] max-md:mt-10"
                     />
-                </Link>
-            </header>
+                    <h1
+                        class="mt-1 max-md:mr-2.5 mx-auto whitespace-nowrap text-white mb-6"
+                    >
+                        VAGABOND
+                    </h1>
 
-            <img
-                src="../../assets/logo/vagabond.svg"
-                alt="Vagabond logo"
-                class="object-contain mx-auto mt-10 max-w-full aspect-[1.63] w-[125px] max-md:mt-10"
-            />
-            <h1
-                class="mt-1 max-md:mr-2.5 mx-auto whitespace-nowrap text-stone-200 mb-6"
-            >
-                VAGABOND
-            </h1>
 
-            <form @submit.prevent="submit">
-                <div
-                    v-if="status"
-                    class="mb-4 font-medium text-sm text-green-600"
-                >
-                    {{ status }}
-                </div>
+                    <form @submit.prevent="submit">
+                        <div
+                            v-if="status"
+                            class="mb-4 font-medium text-sm text-green-600"
+                        >
+                            {{ status }}
+                        </div>
 
-                <div class="mb-6">
-                    <InputLabel for="email" />
-                    <TextInput
-                        id="email"
-                        type="email"
-                        class="px-7 py-4 w-full whitespace-nowrap rounded-[50px] bg-stone-200 text-neutral-600 max-md:px-5 max-md:max-w-full"
-                        v-model="form.email"
-                        required
-                        autofocus
-                        autocomplete="username"
-                        placeholder="Email"
-                    />
-                    <InputError class="mt-2" :message="form.errors.email" />
-                </div>
+                        <div class="mb-6">
+                            <InputLabel for="email" />
+                            <TextInput
+                                id="email"
+                                type="email"
+                                class="px-7 py-4 w-full whitespace-nowrap rounded-[50px] bg-stone-200 text-neutral-600 max-md:px-5 max-md:max-w-full"
+                                v-model="form.email"
+                                required
+                                autofocus
+                                autocomplete="username"
+                                placeholder="Email"
+                            />
+                            <InputError class="mt-2" :message="form.errors.email" />
+                        </div>
 
-                <div class="mb-6 relative">
-                    <InputLabel for="password" />
-                    <TextInput
-                        id="password"
-                        type="password"
-                        class="px-7 py-4 w-full whitespace-nowrap bg-stone-200 rounded-[50px] text-neutral-600 max-md:px-5 max-md:max-w-full"
-                        v-model="form.password"
-                        required
-                        autocomplete="current-password"
-                        placeholder="Password"
-                    />
-                    <InputError class="mt-2" :message="form.errors.password" />
-                </div>
-                <div class="mb-4">
-                    <vue-turnstile
-                        :site-key="TURNSTILE_SITE_KEY"
-                        @success="onTurnstileSuccess"
-                        @expired="onTurnstileExpired"
-                        @error="onTurnstileError"
-                        required
-                        v-model="form.token"
-                    />
+                        <div class="mb-8 relative">
+                            <InputLabel for="password" />
+                            <TextInput
+                                id="password"
+                                type="password"
+                                class="px-7 py-4 w-full whitespace-nowrap bg-stone-200 rounded-[50px] text-neutral-600 max-md:px-5 max-md:max-w-full"
+                                v-model="form.password"
+                                required
+                                autocomplete="current-password"
+                                placeholder="Password"
+                            />
+                            <InputError class="mt-2" :message="form.errors.password" />
+                        </div>
+
+
+                        <div
+                            class="flex flex-wrap mb-4 w-full text-xl max-md:max-w-full justify-center items-center"
+                        >
+                            <div class="flex flex-1 gap-2 text-white mt-2">
+                                <Checkboxs
+                                    name="remember"
+                                    v-model:checked="form.remember"
+                                />
+                                <span class="ml-2.5 -mt-1.5">Remember Me</span>
+                            </div>
+
+                            <Link
+                                v-if="canResetPassword"
+                                :href="route('password.request')"
+                                class="text-right text-neutral-600 transform transition-transform hover:scale-105 hover:text-amber-300"
+                            >
+                                Forgot Password?
+                            </Link>
+                        </div>
+                        <div class="flex justify-center items-center scale-90 mb-2">
+                            <vue-turnstile
+                                :site-key="TURNSTILE_SITE_KEY"
+                                @success="onTurnstileSuccess"
+                                @expired="onTurnstileExpired"
+                                @error="onTurnstileError"
+                                required
+                                v-model="form.token"
+                            />
+                            <div
+                                v-if="form.errors.token || !form.token"
+                                class="text-red-500 text-sm mt-1"
+                            >
+                                {{ form.errors.token }}
+                            </div>
+                        </div>
+                        
+
+                        
+                        <div class="flex mt-4">
+                            <PrimaryButton
+                            class="w-[300px] h-[70px] justify-center mx-auto rounded-xl shadow-md bg-vaga max-w-full text-center text-white whitespace-nowrap max-md:px-5 max-md:mt-10 transform transition-transform hover:scale-105"
+                            :class="{ 'opacity-25': form.processing }"
+                            :disabled="form.processing"
+                        >
+                            Log in
+                        </PrimaryButton>
+                        </div>
+
+                    </form>
+                    
+
                     <div
-                        v-if="form.errors.token || !form.token"
-                        class="text-red-500 text-sm mt-1"
+                        class="flex gap-2.5 justify-center items-center self-center p-2.5 mt-2 text-xl text-center text-white transform transition-transform hover:scale-105"
                     >
-                        {{ form.errors.token }}
+                        <p class="self-stretch my-auto">New to Vagabond?</p>
+                        <div class="text-bond hover:text-amber-300">
+                            <Link
+                                :href="route('register')"
+                                :active="route().current('register')"
+                            >
+                                Sign Up
+                            </Link>
+                        </div>
                     </div>
-                </div>
-                <div
-                    class="flex flex-wrap gap-10 w-full text-xl max-md:max-w-full"
-                >
-                    <div class="flex flex-1 gap-2.5 text-white">
-                        <Checkboxs
-                            name="remember"
-                            v-model:checked="form.remember"
-                        />
-                        <span class="ml-2.5 -mt-1">Remember Me</span>
-                    </div>
-                    <Link
-                        v-if="canResetPassword"
-                        :href="route('password.request')"
-                        class="text-right text-neutral-600"
-                    >
-                        Forgot Password?
-                    </Link>
-                </div>
+                </section>
+            </main>
+        </div>
+    </div>
 
-                <PrimaryButton
-                    class="w-[487px] h-[70px] justify-center rounded-[50px] bg-vaga mt-8 ml-8 max-w-full text-center text-white whitespace-nowrap max-md:px-5 max-md:mt-10"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
-                    Log in
-                </PrimaryButton>
-            </form>
-
-            <div
-                class="flex gap-2.5 justify-center items-center self-center p-2.5 mt-2 text-xl text-center text-white"
-            >
-                <p class="self-stretch my-auto">New to Vagabond?</p>
-                <div class="text-bond hover:text-amber-500">
-                    <Link
-                        :href="route('register')"
-                        :active="route().current('register')"
-                    >
-                        Sign Up
-                    </Link>
-                </div>
-            </div>
-        </section>
-    </main>
 </template>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -55,129 +55,141 @@ const submit = () => {
 <template>
     <Head title="Register" />
     <img src="../../assets/home.png" class="absolute w-screen h-screen z-10" />
-    <main
-        class="flex shadow-lg absolute inset-0 bg-white/20 backdrop-blur-3xl ml-[750px] mt-screen rounded-[30px] w-[700px] h-[700px] m-auto z-50"
-    >
-        <section
-            class="flex flex-col px-16 mb-0 w-full text-2xl leading-none max-md:px-5 h-[100px] rounded-[50px]"
+    <div class="flex flex-row w-screen h-screen">
+    <div class="flex w-1/2">
+        <!-- Left content -->
+    </div>
+    <div class="flex w-1/2 justify-center items-center">
+        <main
+            class="flex shadow-lg bg-white/20 backdrop-blur-3xl rounded-[30px] w-[700px] h-[700px] z-50"
         >
-            <header
-                class="flex flex-row items-end -ml-10 mt-4 max-w-full text-2xl leading-none w-[89px]"
-            >
-                <Link :href="route('home')" :active="route().current('home')">
-                    <img
-                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/2cd9ddf8843928bbd676bc009af63527de529dc32fabc9853b551aff413055c9?placeholderIfAbsent=true&apiKey=ed89e008b7de47e5a3187e8a4c1c72f3"
-                        alt=""
-                        class="object-contain self-start w-12 aspect-[0.94]"
-                    />
-                </Link>
-            </header>
-
-            <img
-                src="../../assets/logo/vagabond.svg"
-                alt="Vagabond logo"
-                class="object-contain mx-auto mt-10 max-w-full aspect-[1.63] w-[125px] max-md:mt-10"
-            />
-            <h1
-                class="mt-1 max-md:mr-2.5 mx-auto whitespace-nowrap text-stone-200 mb-6"
-            >
-                VAGABOND
-            </h1>
-
-                    <form @submit.prevent="submit" class="space-y-6">
-                        <div>
-                            <InputLabel for="name" />
-                            <TextInput
-                                id="name"
-                                type="text"
-                                class="px-7 py-4 w-full whitespace-nowrap bg-stone-200 rounded-[50px] text-neutral-600 max-md:px-5 max-md:max-w-full"
-                                v-model="form.name"
-                                required
-                                autofocus
-                                autocomplete="name"
-                                placeholder="Name"
+                <section
+                    class="flex flex-col px-16 mb-0 w-full text-2xl leading-none max-md:px-5 h-[100px] rounded-[50px]"
+                >
+                    <header
+                        class="flex items-end  -ml-10 mt-4 max-w-full text-2xl leading-none w-[89px]"
+                    >
+                        <Link :href="route('home')" :active="route().current('home')">
+                            <img
+                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/2cd9ddf8843928bbd676bc009af63527de529dc32fabc9853b551aff413055c9?placeholderIfAbsent=true&apiKey=ed89e008b7de47e5a3187e8a4c1c72f3"
+                                alt=""
+                                class="object-contain self-start w-12 pt-"
                             />
-                            <InputError class="mt-2" :message="form.errors.name" />
+                        </Link>
+                    </header>
+
+                    <img
+                        src="../../assets/logo/vagabond.svg"
+                        alt="Vagabond logo"
+                        class="object-contain mx-auto -mt-4 max-w-full aspect-[1.63] w-[125px] max-md:mt-10"
+                    />
+                    <h1
+                        class="mt-2 max-md:mr-2.5 mx-auto whitespace-nowrap text-white mb-6"
+                    >
+                        VAGABOND
+                    </h1>
+
+                            <form @submit.prevent="submit" class="space-y-6">
+                                <div>
+                                    <InputLabel for="name" />
+                                    <TextInput
+                                        id="name"
+                                        type="text"
+                                        class="px-7 py-3 w-full whitespace-nowrap bg-stone-200 rounded-[50px] text-neutral-600 max-md:px-5 max-md:max-w-full"
+                                        v-model="form.name"
+                                        required
+                                        autofocus
+                                        autocomplete="name"
+                                        placeholder="Name"
+                                    />
+                                    <InputError class="mt-2" :message="form.errors.name" />
+                                </div>
+
+                        <div>
+                            <InputLabel for="email" />
+                            <TextInput
+                                id="email"
+                                type="email"
+                                class="px-7 py-3 w-full whitespace-nowrap bg-stone-200 rounded-[50px] text-neutral-600 max-md:px-5 max-md:max-w-full"
+                                v-model="form.email"
+                                required
+                                autocomplete="username"
+                                placeholder="Email"
+                            />
+                            <InputError class="mt-2" :message="form.errors.email" />
                         </div>
 
-                <div>
-                    <InputLabel for="email" />
-                    <TextInput
-                        id="email"
-                        type="email"
-                        class="px-7 py-4 w-full whitespace-nowrap bg-stone-200 rounded-[50px] text-neutral-600 max-md:px-5 max-md:max-w-full"
-                        v-model="form.email"
-                        required
-                        autocomplete="username"
-                        placeholder="Email"
-                    />
-                    <InputError class="mt-2" :message="form.errors.email" />
-                </div>
+                                <div>
+                                    <InputLabel for="password" />
+                                    <TextInput
+                                        id="password"
+                                        type="password"
+                                        class="px-7 py-3 w-full whitespace-nowrap bg-stone-200 rounded-[50px] text-neutral-600 max-md:px-5 max-md:max-w-full"
+                                        v-model="form.password"
+                                        required
+                                        autocomplete="new-password"
+                                        placeholder="Password"
+                                    />
+                                    <InputError class="mt-2" :message="form.errors.password" />
+                                </div>
 
-                        <div>
-                            <InputLabel for="password" />
+                        <div class="flex">
+                            <InputLabel for="password_confirmation" />
                             <TextInput
-                                id="password"
+                                id="password_confirmation"
                                 type="password"
-                                class="px-7 py-4 w-full whitespace-nowrap bg-stone-200 rounded-[50px] text-neutral-600 max-md:px-5 max-md:max-w-full"
-                                v-model="form.password"
+                                class="px-7 py-3 w-full whitespace-nowrap bg-stone-200 rounded-[50px] text-neutral-600 max-md:px-5 max-md:max-w-full"
+                                v-model="form.password_confirmation"
                                 required
                                 autocomplete="new-password"
-                                placeholder="Password"
+                                placeholder="Confirm Password"
                             />
-                            <InputError class="mt-2" :message="form.errors.password" />
+                            <InputError
+                                class="mt-2"
+                                :message="form.errors.password_confirmation"
+                            />
                         </div>
 
-                <div>
-                    <InputLabel for="password_confirmation" />
-                    <TextInput
-                        id="password_confirmation"
-                        type="password"
-                        class="px-7 py-4 w-full whitespace-nowrap bg-stone-200 rounded-[50px] text-neutral-600 max-md:px-5 max-md:max-w-full"
-                        v-model="form.password_confirmation"
-                        required
-                        autocomplete="new-password"
-                        placeholder="Confirm Password"
-                    />
-                    <InputError
-                        class="mt-2"
-                        :message="form.errors.password_confirmation"
-                    />
-                </div>
+                        <div class=" flex mx-auto justify-center ">
+                            <vue-turnstile
+                                :site-key="TURNSTILE_SITE_KEY"
+                                @success="onTurnstileSuccess"
+                                @expired="onTurnstileExpired"
+                                @error="onTurnstileError"
+                                required
+                                v-model="form.token"
+                            />
+                            <div
+                                v-if="form.errors.token || !form.token"
+                                class="text-red-500 text-sm mt-1"
+                            >
+                                {{ form.errors.token }}
+                            </div>
+                        </div>
 
-                <div class="mb-4">
-                    <vue-turnstile
-                        :site-key="TURNSTILE_SITE_KEY"
-                        @success="onTurnstileSuccess"
-                        @expired="onTurnstileExpired"
-                        @error="onTurnstileError"
-                        required
-                        v-model="form.token"
-                    />
-                    <div
-                        v-if="form.errors.token || !form.token"
-                        class="text-red-500 text-sm mt-1"
-                    >
-                        {{ form.errors.token }}
-                    </div>
-                </div>
+                        <div class="flex flex-col items-center justify-end ">
+                            <PrimaryButton
+                                class="w-[50%] h-[60px] justify-center bg-vaga max-w-full text-center  text-white whitespace-nowrap max-md:px-5 max-md:mt-10  transform transition-transform hover:scale-105"
+                                :class="{ 'opacity-25': form.processing }"
+                                :disabled="form.processing"
+                            >
+                                <div class="text-base">
+                                    Register
+                                </div>
+                                
+                            </PrimaryButton>
+                            <Link
+                                :href="route('login')"
+                                class="text-[18px] mt-5 text-bond hover:text-amber-400 rounded-md  transform transition-transform hover:scale-105"
+                            >
+                                Already have a Vagabond Account?
+                            </Link>
+                        </div>
+                    </form>
+                </section>
+            </main>
 
-                <div class="flex flex-col items-center justify-end">
-                    <PrimaryButton
-                        class="w-[50%] h-[70px] justify-center bg-vaga max-w-full text-center text-white whitespace-nowrap max-md:px-5 max-md:mt-10"
-                        :class="{ 'opacity-25': form.processing }"
-                        :disabled="form.processing"
-                    >
-                        Register
-                    </PrimaryButton>
-                    <Link
-                        :href="route('login')"
-                        class="underline text-[18px] mt-5 text-bond hover:text-amber-400 rounded-md"
-                    >
-                        Already have a Vagabond Account?
-                    </Link>
-                </div>
-            </form>
-        </section>
-    </main>
+        </div>
+    </div>
+
 </template>
