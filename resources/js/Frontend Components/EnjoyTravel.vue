ef56610a5da52305d8d0a36492f6cb9e6ad701f9
Ardya Pusaka <143238697+KizaruZero@users.noreply.github.com>
2024-11-30 17:09:37 +0700

    up front end home view

---
+++ resources/js/Frontend Components/EnjoyTravel.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="relative">
+        <!-- Left Text Section -->
+        <div
+            class="absolute top-1/2 left-10 transform -translate-y-1/2 z-10 text-white max-w-2xl"
+        >
+            <h1 class="text-6xl font-bold mb-4">Enjoy your travel</h1>
+            <p class="text-md font-light text-gray-200">
+                Traveling is a great way to escape the daily routine and
+                experience new cultures and places. Explore the world with us
+                and enjoy your travel.
+            </p>
+        </div>
+
+        <!-- Background Image -->
+        <div class="relative">
+            <img src="@/assets/explore2.png" class="rounded-3xl" />
+            <!-- Floating Label -->
+            <div
+                class="absolute bottom-10 right-10 bg-white text-gray-700 py-2 px-4 rounded-lg"
+            >
+                Gunung Bromo,<br />
+                Jawa Timur
+            </div>
+        </div>
+    </section>
+</template>
---
+++ resources/js/Frontend Components/WhyUsSection.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="relative overflow-hidden">
+        <div class="px-44">
+            <div class="mx-auto text-center">
+                <h1 class="text-3xl font-bold text-white">Why Choose Us?</h1>
+                <p class="text- tracking-wider text-white mt-2">
+                    We strive to make your travel experience effortless and
+                    memorable. Here's why you should choose us for your next
+                </p>
+            </div>
+            <div class="grid grid-cols-4 gap-8 mt-10">
+                <div
+                    class="flex flex-col items-center hover:scale-105 transition-transform duration-300 p-4"
+                    data-aos="fade-right"
+                >
+                    <img
+                        src="@/assets/easy.jpeg"
+                        alt="why-us-1"
+                        class="rounded-full w-1/2"
+                    />
+                    <p class="text-2xl text-white text-center font-bold mt-6">
+                        Easy Booking Process
+                    </p>
+                    <p class="text-sm text-gray-300 mt-2 mx-auto text-center">
+                        Enjoy a seamless and user-friendly booking experience
+                        that lets you secure your tickets in just a few clicks,
+                        anytime, anywhere.
+                    </p>
+                </div>
+                <div
+                    class="flex flex-col items-center hover:scale-105 transition-transform duration-300 p-4"
+                    data-aos="fade-up"
+                >
+                    <img
+                        src="@/assets/murah.jpeg"
+                        alt="why-us-2"
+                        class="rounded-full w-1/2"
+                    />
+                    <p class="text-2xl text-white text-center font-bold mt-6">
+                        Best Price Guarentee
+                    </p>
+                    <p class="text-sm text-gray-300 mt-2 mx-auto text-center">
+                        We provide competitive and transparent pricing with no
+                        hidden fees, ensuring great value for your travel plans.
+                    </p>
+                </div>
+                <div
+                    class="flex flex-col items-center hover:scale-105 transition-transform duration-300 p-4"
+                    data-aos="fade-up"
+                >
+                    <img
+                        src="@/assets/lengkap.jpeg"
+                        alt="why-us-2"
+                        class="rounded-full w-1/2"
+                    />
+                    <p class="text-2xl text-white text-center font-bold mt-6">
+                        Complete Destinations
+                    </p>
+                    <p class="text-sm text-gray-300 mt-2 mx-auto text-center">
+                        Discover a variety of destinations, from popular
+                        hotspots to hidden gems, all available on one platform.
+                    </p>
+                </div>
+                <div
+                    class="flex flex-col items-center hover:scale-105 transition-transform duration-300 p-4"
+                    data-aos="fade-up"
+                >
+                    <img
+                        src="@/assets/cservice.jpeg"
+                        alt="why-us-2"
+                        class="rounded-full w-1/2"
+                    />
+                    <p class="text-2xl text-white text-center font-bold mt-6">
+                        24/7 Customer Support
+                    </p>
+                    <p class="text-sm text-gray-300 mt-2 mx-auto text-center">
+                        Our dedicated support team is available around the clock
+                        to assist you, from pre-booking inquiries to on-trip
+                        assistance.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
